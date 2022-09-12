c19156ff419700423b65ea568ae45c3898d8a774
MargretOG <ogemama903@gmail.com>
2022-09-12 10:44:10 -0700

    test

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-
+#include <time.h>
 /**
- * main- print a string
- * Description: use printf
- * Return: 0
+ * main - my Entry point
+ *
+ * Return: 0 (success)
  */
-
 int main(void)
 {
-	printf("%s",  "with proper grammar, but the outcome is a piece of art,\n");
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+		printf("\n");
+
 	return (0);
-}  
+}
