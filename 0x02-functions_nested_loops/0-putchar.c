bd9a16fff28cab75ca8d0118d8b1b34aa4d2f549
MargretOG <ogemama903@gmail.com>
2022-09-13 15:26:58 -0700

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	char a[] = "_putchar";
+	int b;
+
+	for (b = 0; b < 8; b++)
+	{
+		_putchar(a[b]);
+	}
+		_putchar('\n);
+		return (0);
+}
